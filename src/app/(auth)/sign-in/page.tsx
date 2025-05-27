79a8bf71794f202905923ce0e073e3647918539f
hphuynh <163489455+Harry-Huynh@users.noreply.github.com>
2025-05-27 17:36:49 -0400

    Add UI for sign-in page and Add new form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,9 @@
-export default function SignIn(){
-    return <div>SignIn</div>
+import AuthForm from "@/components/AuthForm";
+
+export default function SignIn() {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
+  );
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,9 @@
-export default function SignUp(){
-    return <div>SignUp</div>
+import AuthForm from "@/components/AuthForm";
+
+export default function SignUp() {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 }
